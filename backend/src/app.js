933bbc2e054f90bb3ac01011560c676e16130446
annetherezaprofissional@gmai.com <annetherezaprofissional@gmail.com>
2023-03-23 08:55:34 -0300

    refactor: app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const cors = require('cors')
 
+const conn = require('./app.js')
+
 const router = require('./router')
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use(router)
+
+conn
+    try {
+        app.listen(5000),   console.log('Conectado.')  
+    } catch (error) {
+        console.log(error)
+    }
 
 module.exports = app
